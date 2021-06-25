aacec08344ed5fbff97fbb78bfd9fba620a1f3e6
Deivid <deivid.magno.nascimento@gmail.com>
2021-06-25 19:34:55 -0300

    feature: like and delete question

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext';
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
 
